da9967860ba9e7235df6ddbb966bb5b95f740e3a
Daniel McCormick <dcaseymccormick@gmail.com>
2022-09-11 18:50:28 -0500

    added correct links

--- script.js
+++ script.js
@@ -1,5 +1,26 @@
-var apikey="4e35ef17dcaf3aef4c3cf979343d2d51"
-function displayWeather(cityName){
-var onecalldata=`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
-var currentweatherdata=`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+var apikey = "4e35ef17dcaf3aef4c3cf979343d2d51";
+function displayWeather(cityName) {
+  var currentweatherdata = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apikey}&units=imperial`;
+
+  fetch(currentweatherdata)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (formattedcurrentweather) {
+      console.log(formattedcurrentweather);
+      var onecalldata = `https://api.openweathermap.org/data/2.5/forecast?lat=${formattedcurrentweather.coord.lat}&lon=${formattedcurrentweather.coord.lon}&appid=${apikey}&units=imperial`;
+
+      fetch(onecalldata)
+      .then(function(response){
+
+        return response.json();
+
+      })
+      .then(function(formattedonecall){
+
+        console.log(formattedonecall)
+      })
+
+    });
 }
+displayWeather("austin")
