32f6e1aeaf331a2406271959dd2be5e2c307b801
Daniel McCormick <dcaseymccormick@gmail.com>
2022-09-11 18:24:32 -0500

    added javascript and api links

---
+++ script.js
@@ -0,0 +1,5 @@
+var apikey="4e35ef17dcaf3aef4c3cf979343d2d51"
+function displayWeather(cityName){
+var onecalldata=`https://api.openweathermap.org/data/2.5/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}`
+var currentweatherdata=`https://api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}`
+}
